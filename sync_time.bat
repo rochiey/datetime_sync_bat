93744610c8072fbabdf18d23a69bbd76ffd4f9a0
rochiey <38701913+rochiey@users.noreply.github.com>
2023-08-07 21:59:32 +0800

    File Creation

---
+++ sync_time.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set retry_count=5
+
+:SYNC_LOOP
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" /v "Type" /d "NTP" /f
+net stop w32time
+net start w32time
+w32tm /resync
+
+if %ERRORLEVEL% equ 0 (
+    echo Time synchronization was successful.
+) else (
+    set /A retry_count-=1
+    if !retry_count! gtr 0 (
+        echo Time synchronization failed. Retrying (%retry_count% attempts left)...
+        ping -n 5 127.0.0.1 > nul 2>&1
+        goto SYNC_LOOP
+    ) else (
+        echo Time synchronization failed after multiple attempts.
+    )
+)
+
+endlocal
